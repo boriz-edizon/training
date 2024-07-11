0f2a0071ca6cf7f935a094bb8fa0c900e1795b2f
boriz-edizon <boris.edison@zeuslearning.com>
2024-07-11 12:40:52 +0530

    starting resizing

--- task-5/index.js
+++ task-5/index.js
@@ -4,9 +4,8 @@ const ctx = canvas.getContext("2d");
 const w1 =80
 const h1 = 30
 
-const rows = 10;
-const column = 10;
-  
+const rows = 50;
+const column = 50;
 
 class cellStruct {
     constructor(xVal, yVal,width, height,text, isClicked,isSelected){
@@ -21,19 +20,15 @@ class cellStruct {
 
     createCell() {
         ctx.strokeRect(this.xVal, this.yVal, w1, h1);
-        
+        // ctx.lineWidth   = 5;
     }
 
-    // updateCell() {
-    //     if(this.isClicked){
-    //         console.log("timeseires")
-    //         // ctx.fillRect(this.xVal, this.yVal, w1, h1);
-    //         // ctx.fillStyle = "black";
-    //         // ctx.fillText(this.text, this.xVal, );
-    //         ctx.font = "16px serif";
-    //         ctx.fillText(this.text, this.xVal , this.yVal + this.height/1.5, 140);
-    //     }
-    // }
+    updateCell() {
+        if(this.isClicked){
+            ctx.font = "16px serif";
+            ctx.fillText(this.text, this.xVal , this.yVal + this.height/1.5, 140);
+        }
+    }
     selectCell() {
         // if(this.isClicked){
         //     ctx.rect(this.xVal, this.yVal, w1, h1);
@@ -62,7 +57,7 @@ var selected = []
 for(let i = 0; i < rows; i++) {
   cells[i] = []
     for(let j = 0; j<column ; j++){
-       cell = new cellStruct(0+j*(w1), 0+i*(h1),w1,h1,' ', false,false)
+       cell = new cellStruct(0+j*(w1), 0+i*(h1),w1,h1,' ', false,0)
        cell.createCell()
        cells[i].push(cell)
     }
@@ -75,91 +70,31 @@ canvas.addEventListener("click", function(e) {
     onCell(mouseX, mouseY)
 });
 
-// function onCell(mouseX, mouseY) {
-//     var xIndex = findX(0, cells[0].length - 1,mouseX)
-//     var yIndex = findY(0, cells.length - 1,mouseY)
-//     clickedCell = cells[yIndex][xIndex]
-//     clickedCell.isClicked = true
+function onCell(mouseX, mouseY) {
+    var xIndex = findX(0, cells[0].length - 1,mouseX)
+    var yIndex = findY(0, cells.length - 1,mouseY)
+    clickedCell = cells[yIndex][xIndex]
+    clickedCell.isClicked = true
 
-//     var cellInput=document.querySelector(".content");
-//     cellInput.value = clickedCell.text
-//     cellInput.style.display = "block";
-//     cellInput.style.top = clickedCell.yVal + 8;
-//     cellInput.style.left = clickedCell.xVal + 8;
-//     cellInput.style.height = clickedCell.height  ;
-//     cellInput.style.width = clickedCell.width  ;
+    var cellInput=document.querySelector(".content");
+    cellInput.value = clickedCell.text
+    cellInput.style.display = "block";
+    cellInput.style.top = clickedCell.yVal ;
+    cellInput.style.left = clickedCell.xVal;
+    cellInput.style.height = clickedCell.height  ;
+    cellInput.style.width = clickedCell.width  ;
     
 
-//     console.log(cellInput.value)
-//     cellInput.onblur = () => {
-//         console.log(cellInput.value)
-//         clickedCell.text = cellInput.value
-//         clickedCell.updateCell()
-//         console.log(cells)
-//     }
-// }
-
-canvas.addEventListener("mousedown", function(e) {
-    var cRect = canvas.getBoundingClientRect();        
-    var mouseX = Math.round(e.offsetX - cRect.x +8);
-    var mouseY = Math.round(e.offsetY - cRect.top +8);
-    selectCell = selectCells(mouseX, mouseY)
-    selectCell.isSelected = true
-    selectCell.selectCell()
-    selected.push(selectCell)
-
-
-    var startX = findX(0, cells[0].length - 1,mouseX)
-    var startY = findY(0, cells.length - 1,mouseY)
-    mouseMove(startX, startY,selectCell);
-})
-
-// canvas.addEventListener("mouseup", function(e) {
-//     (selected).forEach(cell => {
-//         cell.isClicked = false;
-//         cell.selectCell()
-//     });
-//     selected = []
-// })
-
-const mouseMove = (startX, startY,selectCell) => {
-    canvas.addEventListener("mousemove", function(e) {
-    
-        var cRect = canvas.getBoundingClientRect();        
-        var mouseX = Math.round(e.offsetX - cRect.x +8);
-        var mouseY = Math.round(e.offsetY - cRect.top +8);
-        // console.log(mouseX,mouseY)
-        var xIndex = findX(0, cells[0].length - 1,mouseX)
-        var yIndex = findY(0, cells.length - 1,mouseY)
-        // console.log(xIndex,yIndex)
-        for(let i = 0; i<selected.length ; i++){
-            console.log("This is from outer for loop")
-            console.log(selected[i].isSelected)
-           
-            selected[i].isSelected = false;
-            console.log(selected[i].isSelected)
-            console.log(selected[i]);
-            selected[i].selectCell();
-        }
-       selected = []
-        for(var i=startY; i<=yIndex; i++){
-            for(var j=startX; j<=xIndex; j++){
-                console.log(i,j,yIndex,xIndex)
-                if(!(cells[i][j].isSelected)){
-                    selected.push(cells[i][j])
-                    console.log("This is from inner for loop")
-                    console.log(cells[i][j])
-                    console.log(cells[i][j].isSelected)
-                    cells[i][j].isSelected = true;
-                    console.log(cells[i][j].isSelected)
-
-                    cells[i][j].selectCell();
-                }
-            }
-        }
-    })
-
+    cellInput.focus()
+    cellInput.onblur = () => {
+        console.log(cellInput.value)
+        clickedCell.text = cellInput.value
+        clickedCell.updateCell()
+        console.log(cells)
+    }
 }
+
+
 
 function selectCells(mouseX,mouseY) {
     var xIndex = findX(0, cells[0].length - 1,mouseX)
@@ -192,3 +127,74 @@ function findY(frontCell, lastCell,mouseY) {
         return findY(midindex + 1, lastCell,mouseY)     
     }
 }
+
+
+
+
+
+
+
+
+
+
+// canvas.addEventListener("mousedown", function(e) {
+//     var cRect = canvas.getBoundingClientRect();        
+//     var mouseX = Math.round(e.offsetX - cRect.x +8);
+//     var mouseY = Math.round(e.offsetY - cRect.top +8);
+//     selectCell = selectCells(mouseX, mouseY)
+//     selectCell.isSelected = true
+//     selectCell.selectCell()
+//     selected.push(selectCell)
+
+
+//     var startX = findX(0, cells[0].length - 1,mouseX)
+//     var startY = findY(0, cells.length - 1,mouseY)
+//     mouseMove(startX, startY,selectCell);
+// })
+
+// canvas.addEventListener("mouseup", function(e) {
+//     (selected).forEach(cell => {
+//         cell.isClicked = false;
+//         cell.selectCell()
+//     });
+//     selected = []
+// })
+
+// const mouseMove = (startX, startY,selectCell) => {
+//     canvas.addEventListener("mousemove", function(e) {
+    
+//         var cRect = canvas.getBoundingClientRect();        
+//         var mouseX = Math.round(e.offsetX - cRect.x +8);
+//         var mouseY = Math.round(e.offsetY - cRect.top +8);
+//         // console.log(mouseX,mouseY)
+//         var xIndex = findX(0, cells[0].length - 1,mouseX)
+//         var yIndex = findY(0, cells.length - 1,mouseY)
+//         // console.log(xIndex,yIndex)
+//         for(let i = 0; i<selected.length ; i++){
+//             console.log("This is from outer for loop")
+//             console.log(selected[i].isSelected)
+           
+//             selected[i].isSelected = 0;
+//             console.log(selected[i].isSelected)
+//             console.log(selected[i]);
+//             selected[i].selectCell();
+//         }
+//        selected = []
+//         for(var i=startY; i<=yIndex; i++){
+//             for(var j=startX; j<=xIndex; j++){
+//                 console.log(i,j,yIndex,xIndex)
+//                 if(!(cells[i][j].isSelected)){
+//                     selected.push(cells[i][j])
+//                     console.log("This is from inner for loop")
+//                     console.log(cells[i][j].isSelected)
+//                     console.log(cells[i][j])
+//                     cells[i][j].isSelected = 1;
+//                     console.log(cells[i][j].isSelected)
+//                     console.log(cells[i][j])
+//                     setTimeout((cells[i][j].selectCell()),200)
+//                 }
+//             }
+//         }
+//     })
+
+// }
